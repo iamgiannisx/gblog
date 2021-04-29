451a8d566d4738a16ff424de92575dd02a351247
Giannis Theoropoulos <jtheogr@gmail.com>
2021-04-29 12:05:10 +0300

    Change author name

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Giannis Theodoropoulos`,
+      summary: `who lives and works in Athens Greece building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `iamgiannisx`,
     },
   },
   plugins: [
